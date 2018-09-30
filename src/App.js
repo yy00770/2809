d20d7ba58657c38a2a7514fd67e0ba059d7c4352
Yang Yang <598752625@qq.com>
2018-09-30 11:04:41 +1000

    Add dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+
 
 class App extends Component {
   render() {
@@ -9,9 +11,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Dashboard/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+         
         </p>
       </div>
     );
